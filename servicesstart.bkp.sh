cff25c96d924259adfb35693885ccdb01539a4d5
root <you@example.com>
2022-09-28 19:05:48 +0530

    updated files2

--- servicesstart.bkp.sh
+++ servicesstart.bkp.sh
@@ -4,6 +4,7 @@ service elasticsearch start
 service php7.3-fpm start
 service mysql start
 cd /var/www/html
+clear composer cache
 composer create-project --no-interaction --repository-url=https://repo.magento.com/ magento/project-community-edition=2.3.5 /var/www/html/magento
 cd /var/www/html/magento
 find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
@@ -25,17 +26,8 @@ bin/magento setup:install \
 --currency=USD \
 --timezone=America/Chicago \
 --use-rewrites=1 \
---search-engine=elasticsearch7 \
---elasticsearch-host=localhost \
---elasticsearch-port=9200
 cp /var/www/html/env.php /var/www/html/magento/app/etc/env.php
-cp /root/.config/composer/auth.json /var/www/html/magento/var/composer_home/auth.json
-php bin/magento setup:upgrade
-php bin/magento setup:di:compile
-php bin/magento setup:static-content:deploy -f
-php bin/magento cache:clean
-php bin/magento cache:flush
-php bin/magento sampledata:deploy
+#cp /root/.composer/auth.json /var/www/html/magento/var/composer_home/auth.json
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 php bin/magento setup:static-content:deploy -f
